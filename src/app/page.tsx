bb217b49cea2c1351e48a5576d0f42ffd42238d6
Le Minh Hung <hungleminh371@gmail.com>
2024-09-18 15:16:24 +0700

    [AMIRAS-HUNG-7] feat: Update Reason, Customer, Event section structures

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,8 +50,8 @@ export default function HomePage() {
 				</div>
 
 			</div>
-			<div className='application-section'>
-				<div className='application-board container'>
+			<div className='app section p1'>
+				<div className='app-board container section'>
 					<h1>NHIỀU LĨNH VỰC ỨNG DỤNG SMART LOYALTY</h1>
 					<div className='app-list'>
 						<div className='app-card col-inner'>
@@ -84,37 +84,250 @@ export default function HomePage() {
 
 			</div>
 
-			<div className='fundamental-board container section'>
-				<h1>NHIỀU LĨNH VỰC ỨNG DỤNG SMART LOYALTY</h1>
-				<div className='app-list'>
-					<div className='app-card col-inner'>
-						<Image src='/assets/Home/Bank.png' width={90} height={90} alt='' />
-						<p>Tài chính - Ngân hàng</p>
+			<div className='p1'>
+				<div className='fundamental container'>
+					<div className='fundamental-title'>
+						<h1>NỀN TẢNG SMART LOYALTY</h1>
+						<h3>Dễ dàng thiết lập chương trình & đánh giá hiệu quả Loyalty Marketing</h3>
 					</div>
-					<div className='app-card col-inner'>
-						<Image src='/assets/Home/Food.png' width={90} height={90} alt='' />
-						<p>Dịch vụ ăn uống (FnB)</p>
-					</div>
-					<div className='app-card col-inner'>
-						<Image src='/assets/Home/Shopping.png' width={90} height={90} alt='' />
-						<p>Bán lẻ</p>
-					</div>
-					<div className='app-card col-inner'>
-						<Image src='/assets/Home/Health.png' width={90} height={90} alt='' />
-						<p>Sức khỏe - Làm đẹp</p>
-					</div>
-					<div className='app-card col-inner'>
-						<Image src='/assets/Home/Hotel.png' width={90} height={90} alt='' />
-						<p>Du lịch - Khách sạn</p>
-					</div>
-					<div className='app-card col-inner'>
-						<Image src='/assets/Home/Coop.png' width={90} height={90} alt='' />
-						<p>B2B</p>
+					<div className='fundamental-content'>
+						<div className="icon-box">
+							<div className='icon-box-img'>
+								<Image
+									src='/assets/Home/CRM_2.png'
+									fill
+									alt=''
+									style={{ objectFit: 'cover' }}
+								/>
+							</div>
+							<div className='icon-box-details col'>
+								<div className="col-inner">
+									<h3>Quản trị chương trình Loyalty </h3>
+									<p>Tùy chỉnh cơ chế tích điểm.</p>
+									<p>Trả thưởng, cơ chế phân hạng để kết nối với hội viên theo cách của bạn.</p>
+									<p>Thấu hiểu khách hàng thông qua những thông tin giao dịch, tương tác…</p>
+									<p>Cập nhật theo thời gian thực.</p>
+									<Button>Chi tiết</Button>
+								</div>
+							</div>
+						</div>
+						<div className="icon-box">
+
+							<div className='icon-box-details col'>
+								<div className="col-inner">
+									<h3>Quản trị chương trình Loyalty </h3>
+									<p>Trả thưởng, cơ chế phân hạng để kết nối với hội viên theo cách của bạn.</p>
+									<p>Thấu hiểu khách hàng thông qua những thông tin giao dịch, tương tác…</p>
+									<p>Cập nhật theo thời gian thực.</p>
+									<Button>Chi tiết</Button>
+								</div>
+							</div>
+							<div className='icon-box-img'>
+								<Image
+									src='/assets/Home/CRM_2.png'
+									fill
+									alt=''
+									style={{ objectFit: 'cover' }}
+								/>
+							</div>
+						</div>
+						<div className="icon-box">
+							<div className='icon-box-img'>
+								<Image
+									src='/assets/Home/CRM_2.png'
+									fill
+									alt=''
+									style={{ objectFit: 'cover' }}
+								/>
+							</div>
+							<div className='icon-box-details col'>
+								<div className="col-inner">
+									<h3>Quản trị chương trình Loyalty </h3>
+									<p>Tùy chỉnh cơ chế tích điểm.</p>
+									<p>Trả thưởng, cơ chế phân hạng để kết nối với hội viên theo cách của bạn.</p>
+									<p>Thấu hiểu khách hàng thông qua những thông tin giao dịch, tương tác…</p>
+									<Button>Chi tiết</Button>
+								</div>
+							</div>
+						</div>
+						<div className="icon-box">
+
+							<div className='icon-box-details col'>
+								<div className="col-inner">
+									<h3>Quản trị chương trình Loyalty </h3>
+									<p>Tùy chỉnh cơ chế tích điểm.</p>
+									<p>Cập nhật theo thời gian thực.</p>
+									<Button>Chi tiết</Button>
+								</div>
+							</div>
+							<div className='icon-box-img'>
+								<Image
+									src='/assets/Home/CRM_2.png'
+									fill
+									alt=''
+									style={{ objectFit: 'cover' }}
+								/>
+							</div>
+						</div>
+						<div className="icon-box">
+							<div className='icon-box-img'>
+								<Image
+									src='/assets/Home/CRM_2.png'
+									fill
+									alt=''
+									style={{ objectFit: 'cover' }}
+								/>
+							</div>
+							<div className='icon-box-details col'>
+								<div className="col-inner">
+									<h3>Quản trị chương trình Loyalty </h3>
+									<p>Tùy chỉnh cơ chế tích điểm.</p>
+									<p>Trả thưởng, cơ chế phân hạng để kết nối với hội viên theo cách của bạn.</p>
+									<p>Cập nhật theo thời gian thực.</p>
+									<Button>Chi tiết</Button>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
-				<Button>Đăng ký tư vấn</Button>
-			</div>
-		</div >
+			</div >
 
+			<div className='reason p1'>
+				<div className='reason-board container section'>
+					<h1>TẠI SAO DOANH NGHIỆP LỰA CHỌN SMART LOYALTY?</h1>
+					<div className='reason-list'>
+						<div className='reason-card col-inner'>
+							<Image src='/assets/Home/Bank.png' width={30} height={90} alt='' />
+							<h3>GIẢI PHÁP TOÀN DIỆN</h3>
+							<h4>Tư vấn – Đào tạo – Công cụ</h4>
+							<p>Tài chính - Ngân hàng</p>
+						</div>
+						<div className='reason-card col-inner'>
+							<Image src='/assets/Home/Bank.png' width={30} height={90} alt='' />
+							<h3>GIẢI PHÁP TOÀN DIỆN</h3>
+							<h4>Tư vấn – Đào tạo – Công cụ</h4>
+							<p>Tài chính - Ngân hàng</p>
+						</div>
+						<div className='reason-card col-inner'>
+							<Image src='/assets/Home/Bank.png' width={30} height={90} alt='' />
+							<h3>GIẢI PHÁP TOÀN DIỆN</h3>
+							<h4>Tư vấn – Đào tạo – Công cụ</h4>
+							<p>Tài chính - Ngân hàng</p>
+						</div>
+						<div className='reason-card col-inner'>
+							<Image src='/assets/Home/Bank.png' width={30} height={90} alt='' />
+							<h3>GIẢI PHÁP TOÀN DIỆN</h3>
+							<h4>Tư vấn – Đào tạo – Công cụ</h4>
+							<p>Tài chính - Ngân hàng</p>
+						</div>
+						<div className='reason-card col-inner'>
+							<Image src='/assets/Home/Bank.png' width={30} height={90} alt='' />
+							<h3>GIẢI PHÁP TOÀN DIỆN</h3>
+							<h4>Tư vấn – Đào tạo – Công cụ</h4>
+							<p>Tài chính - Ngân hàng</p>
+						</div>
+						<div className='reason-card col-inner'>
+							<Image src='/assets/Home/Bank.png' width={30} height={90} alt='' />
+							<h3>GIẢI PHÁP TOÀN DIỆN</h3>
+							<h4>Tư vấn – Đào tạo – Công cụ</h4>
+
+							<p>Tài chính - Ngân hàng</p>
+						</div>
+
+					</div>
+				</div>
+			</div>
+			<div className='customer p1'>
+				<div className='reason-board container section'>
+					<h1>TẠI SAO DOANH NGHIỆP LỰA CHỌN SMART LOYALTY?</h1>
+					<div className='reason-list'>
+						<div className='reason-card col-inner'>
+							<Image src='/assets/Home/Bank.png' width={30} height={90} alt='' />
+							<h3>GIẢI PHÁP TOÀN DIỆN</h3>
+							<h4>Tư vấn – Đào tạo – Công cụ</h4>
+							<p>Tài chính - Ngân hàng</p>
+						</div>
+						<div className='reason-card col-inner'>
+							<Image src='/assets/Home/Bank.png' width={30} height={90} alt='' />
+							<h3>GIẢI PHÁP TOÀN DIỆN</h3>
+							<h4>Tư vấn – Đào tạo – Công cụ</h4>
+							<p>Tài chính - Ngân hàng</p>
+						</div>
+						<div className='reason-card col-inner'>
+							<Image src='/assets/Home/Bank.png' width={30} height={90} alt='' />
+							<h3>GIẢI PHÁP TOÀN DIỆN</h3>
+							<h4>Tư vấn – Đào tạo – Công cụ</h4>
+							<p>Tài chính - Ngân hàng</p>
+						</div>
+						<div className='reason-card col-inner'>
+							<Image src='/assets/Home/Bank.png' width={30} height={90} alt='' />
+							<h3>GIẢI PHÁP TOÀN DIỆN</h3>
+							<h4>Tư vấn – Đào tạo – Công cụ</h4>
+							<p>Tài chính - Ngân hàng</p>
+						</div>
+						<div className='reason-card col-inner'>
+							<Image src='/assets/Home/Bank.png' width={30} height={90} alt='' />
+							<h3>GIẢI PHÁP TOÀN DIỆN</h3>
+							<h4>Tư vấn – Đào tạo – Công cụ</h4>
+							<p>Tài chính - Ngân hàng</p>
+						</div>
+						<div className='reason-card col-inner'>
+							<Image src='/assets/Home/Bank.png' width={30} height={90} alt='' />
+							<h3>GIẢI PHÁP TOÀN DIỆN</h3>
+							<h4>Tư vấn – Đào tạo – Công cụ</h4>
+
+							<p>Tài chính - Ngân hàng</p>
+						</div>
+
+					</div>
+				</div>
+			</div>
+			<div className='event p1'>
+				<div className='reason-board container section'>
+					<h1>TẠI SAO DOANH NGHIỆP LỰA CHỌN SMART LOYALTY?</h1>
+					<div className='reason-list'>
+						<div className='reason-card col-inner'>
+							<Image src='/assets/Home/Bank.png' width={30} height={90} alt='' />
+							<h3>GIẢI PHÁP TOÀN DIỆN</h3>
+							<h4>Tư vấn – Đào tạo – Công cụ</h4>
+							<p>Tài chính - Ngân hàng</p>
+						</div>
+						<div className='reason-card col-inner'>
+							<Image src='/assets/Home/Bank.png' width={30} height={90} alt='' />
+							<h3>GIẢI PHÁP TOÀN DIỆN</h3>
+							<h4>Tư vấn – Đào tạo – Công cụ</h4>
+							<p>Tài chính - Ngân hàng</p>
+						</div>
+						<div className='reason-card col-inner'>
+							<Image src='/assets/Home/Bank.png' width={30} height={90} alt='' />
+							<h3>GIẢI PHÁP TOÀN DIỆN</h3>
+							<h4>Tư vấn – Đào tạo – Công cụ</h4>
+							<p>Tài chính - Ngân hàng</p>
+						</div>
+						<div className='reason-card col-inner'>
+							<Image src='/assets/Home/Bank.png' width={30} height={90} alt='' />
+							<h3>GIẢI PHÁP TOÀN DIỆN</h3>
+							<h4>Tư vấn – Đào tạo – Công cụ</h4>
+							<p>Tài chính - Ngân hàng</p>
+						</div>
+						<div className='reason-card col-inner'>
+							<Image src='/assets/Home/Bank.png' width={30} height={90} alt='' />
+							<h3>GIẢI PHÁP TOÀN DIỆN</h3>
+							<h4>Tư vấn – Đào tạo – Công cụ</h4>
+							<p>Tài chính - Ngân hàng</p>
+						</div>
+						<div className='reason-card col-inner'>
+							<Image src='/assets/Home/Bank.png' width={30} height={90} alt='' />
+							<h3>GIẢI PHÁP TOÀN DIỆN</h3>
+							<h4>Tư vấn – Đào tạo – Công cụ</h4>
+
+							<p>Tài chính - Ngân hàng</p>
+						</div>
+
+					</div>
+				</div>
+			</div>
+
+		</div>
 	);
 }
--- src/app/san-pham/page.tsx
+++ src/app/san-pham/page.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import './styles.sass';
 
 const page = () => {
     return (
-        <div>page</div>
+        <div className='section'>
+            <div className='banner'>
+
+            </div>
+        </div>
     )
 }
 
