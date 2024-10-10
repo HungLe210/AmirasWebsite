f56976205abb768f734cf03b6e0bdf2d1be65bdf
Le Minh Hung <hungleminh371@gmail.com>
2024-10-11 00:48:31 +0700

    oh no

--- src/hooks/useScroll.ts
+++ src/hooks/useScroll.ts
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 
 export function useScroll(
     threshold: number,
+    key?: string
 ) {
     const [attribute, setAttribute] = useState(false);
     useEffect(() => {
