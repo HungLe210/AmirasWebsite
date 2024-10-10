ab8e5dd12e5cdd13a82bbd2115be0b3cd055b474
Le Minh Hung <hungleminh371@gmail.com>
2024-10-11 00:39:33 +0700

    fix 3

--- src/hooks/useMultiSampleFetcher.ts
+++ src/hooks/useMultiSampleFetcher.ts
@@ -1,28 +1,28 @@
-import { Any, MultiDataFetcherHookFields, MultiDataFetcherHookParams } from '@types';
+// import { Any, MultiDataFetcherHookFields, MultiDataFetcherHookParams } from '@types';
 
-export function useMultiSampleFetcher({}: MultiDataFetcherHookParams<Any>): MultiDataFetcherHookFields<
-	Any,
-	Any
-> {
-	// Call API here
+// export function useMultiSampleFetcher({}: MultiDataFetcherHookParams<Any>): MultiDataFetcherHookFields<
+// 	Any,
+// 	Any
+// > {
+// 	// Call API here
 
-	return {
-		loading: true,
-		data: {
-			page: 1,
-			items: [] as Any[],
-			totalItem: 0,
-			totalPage: 0,
-		},
-		filters: null,
-		changeFilters: (changes: Partial<Any>) => {
-			console.log('Changes: ', changes);
-		},
-		goToPage: (page: number) => {
-			console.log('Page: ', page);
-		},
-		fetchData: () => {
-			//
-		},
-	};
-}
+// 	return {
+// 		loading: true,
+// 		data: {
+// 			page: 1,
+// 			items: [] as Any[],
+// 			totalItem: 0,
+// 			totalPage: 0,
+// 		},
+// 		filters: null,
+// 		changeFilters: (changes: Partial<Any>) => {
+// 			console.log('Changes: ', changes);
+// 		},
+// 		goToPage: (page: number) => {
+// 			console.log('Page: ', page);
+// 		},
+// 		fetchData: () => {
+// 			//
+// 		},
+// 	};
+// }
